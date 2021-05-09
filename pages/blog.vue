a3609b9811c584fcdcb84ae72f458f05745cc7ad
Caceresenzo <caceresenzo1502@gmail.com>
2021-05-09 23:24:27 +0200

    feat(blog): Added a maybe page

---
+++ pages/blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row>
+    <v-col class="text-center"> maybe one day </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Blog',
+      description: 'Some posts I write for anyone to read',
+    }
+  },
+}
+</script>
