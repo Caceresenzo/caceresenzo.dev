1c31a39424e490e2ae3540649ae5b2b93c4d78c5
Caceresenzo <caceresenzo1502@gmail.com>
2021-05-09 23:34:05 +0200

    add: Footer links

---
+++ components/FooterLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-tooltip top>
+    <template #activator="{ on, attrs }">
+      <v-btn
+        color="primary"
+        dark
+        icon
+        link
+        target="_blank"
+        :href="url"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon> mdi-{{ icon }} </v-icon>
+      </v-btn>
+    </template>
+    <span>{{ description }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    description: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -18,13 +18,43 @@
         <v-btn text class="ml-2" nuxt to="/resume"> resume </v-btn>
       </v-container>
     </v-app-bar>
-    <v-main>
+    <v-main class="mb-10">
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
     <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-spacer />
+      <footer-link
+        v-for="(link, index) in footerLinks"
+        :key="index"
+        v-bind="link"
+      />
+      <v-spacer />
     </v-footer>
   </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    footerLinks: [
+      {
+        description: 'Send me an email',
+        icon: 'mail',
+        url: 'mailto:caceresenzo1502@gmail.com',
+      },
+      {
+        description: 'See my projects',
+        icon: 'github',
+        url: 'https://github.com/Caceresenzo',
+      },
+      {
+        description: 'Linked-in',
+        icon: 'linkedin',
+        url: 'https://www.linkedin.com/in/enzo-caceres/',
+      },
+    ],
+  }),
+}
+</script>
